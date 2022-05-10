bd163974105d201fb9489ab54cdfaf3285c9f0a6
JanosVargaBME <var.janos.hu@gmail.com>
2022-05-10 10:55:49 +0200

    Dropbox added

---
+++ DriveClient/Services/DropBoxService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Dropbox.Api;
+
+namespace DriveClient.Services
+{
+    internal class DropBoxService
+    {
+        private string accessToken = "";
+
+        public string actualUser = "";
+
+        public static DropBoxService Instance { get; private set; } = new DropBoxService();
+
+        public bool SetAccessToken(string token)
+        {
+            //TODO
+            return true;
+        }
+        
+        public async Task GetUserName()
+        {
+            using(var db = new DropboxClient(accessToken))
+            {
+                var id = await db.Users.GetCurrentAccountAsync();
+
+                actualUser = id.Name.DisplayName;
+            }
+        }
+    }
+}
